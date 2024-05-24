0a78cab5b65cc02e3d10e6bdcc94af95ee557820
Devin Price <devin@nowyoujump.com>
2024-05-24 04:19:54 -0700

    operation type

--- src/modifiers/stat_modifier.cpp
+++ src/modifiers/stat_modifier.cpp
@@ -5,16 +5,37 @@
 
 using namespace godot;
 
+#define GET_SET_PROPERTY_IMPL(ClassName, Type, Name) \
+Type ClassName::get_##Name() const { \
+    return Name; \
+} \
+void ClassName::set_##Name(const Type p_##Name) { \
+    ##Name = p_##Name; \
+}
+
+#define BIND_GET_SET_METHOD(ClassName, Name) \
+    ClassDB::bind_method(D_METHOD("get_" #Name), &##ClassName::get_##Name); \
+    ClassDB::bind_method(D_METHOD("set_" #Name, "p_" #Name), &##ClassName::set_##Name);
+
+#define BIND_GET_SET(ClassName, Name, VariantType) \
+    BIND_GET_SET_METHOD(ClassName, Name) \
+    ClassDB::add_property(#ClassName, PropertyInfo(VariantType, #Name), "set_" #Name, "get_" #Name);
+
+#define BIND_GET_SET_RESOURCE(ClassName, Name, Type) \
+    BIND_GET_SET_METHOD(ClassName, Name) \
+    ClassDB::add_property(#ClassName, PropertyInfo(Variant::OBJECT, #Name, PROPERTY_HINT_RESOURCE_TYPE, #Type), "set_" #Name, "get_" #Name);
+
+#define BIND_GET_SET_ENUM(ClassName, Name, Values) \
+    BIND_GET_SET_METHOD(ClassName, Name) \
+    ClassDB::add_property(#ClassName, PropertyInfo(Variant::INT, #Name, PROPERTY_HINT_ENUM, Values), "set_" #Name, "get_" #Name);
+
 void StatModifier::_bind_methods() {
-    ClassDB::bind_method(D_METHOD("get_stat"), &StatModifier::get_stat);
-    ClassDB::bind_method(D_METHOD("set_stat", "p_stat"), &StatModifier::set_stat);
-    ClassDB::add_property("StatModifier", PropertyInfo(Variant::OBJECT, "stat", PROPERTY_HINT_RESOURCE_TYPE, "GameplayStat"), "set_stat", "get_stat");
+    BIND_ENUM_CONSTANT(Offset)
+    BIND_ENUM_CONSTANT(Multiply)
+    BIND_ENUM_CONSTANT(Override)
+    BIND_GET_SET_ENUM(StatModifier, operation, "Offset:0,Multiply:1,Override:2")
+    BIND_GET_SET_RESOURCE(StatModifier, stat, GameplayStat)
 }
 
-Ref<GameplayStat> StatModifier::get_stat() const {
-    return stat;
-}
-
-void StatModifier::set_stat(const Ref<GameplayStat> p_stat) {
-    stat = p_stat;
-}
+GET_SET_PROPERTY_IMPL(StatModifier, Ref<GameplayStat>, stat)
+GET_SET_PROPERTY_IMPL(StatModifier, StatModifier::Operation, operation)
--- src/modifiers/stat_modifier.h
+++ src/modifiers/stat_modifier.h
@@ -4,22 +4,35 @@
 #include <godot_cpp/classes/ref.hpp>
 #include <godot_cpp/classes/resource.hpp>
 
-#include "../stats/gameplay_stat.h"
+#include "stats/gameplay_stat.h"
 
 using namespace godot;
+
+#define GET_SET_PROPERTY(Type, Name) \
+private: \
+    Type Name; \
+public: \
+    Type get_##Name() const; \
+    void set_##Name(const Type p_##Name); \
+private:
 
 class StatModifier : public Resource {
     GDCLASS(StatModifier, Resource)
 
-private:
-    Ref<GameplayStat> stat;
-
 public:
-    Ref<GameplayStat> get_stat() const;
-    void set_stat(const Ref<GameplayStat> p_stat);
+    enum Operation {
+        Offset = 0,
+        Multiply = 1,
+        Override = 2,
+    };
+
+GET_SET_PROPERTY(Operation, operation)
+GET_SET_PROPERTY(Ref<GameplayStat>, stat)
 
 protected:
     static void _bind_methods();
 };
+
+VARIANT_ENUM_CAST(StatModifier::Operation)
 
 #endif
